d2f3e669ea4851d3948d213fa9a86e196c3f39d0
bennysolo <78677744+bennysolo@users.noreply.github.com>
2021-04-21 01:13:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ benny.on('group-participants-update', async (anu) => {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
 				teks = `${txtwl}`
-				buff = await getBaper(ppimg)
+				buff = await getBuffer(ppimg)
 				datae = await imageToBase64(JSON.stringify(ppimg).replace(/\"/gi, ''))
 					fs.writeFileSync('janckuk.jpeg', datae, 'base64')
 					var imgbb = require('imgbb-uploader')
 					data = await imgbb("cedeb44b8d204947a6833ca1412ca77d", 'janckuk.jpeg')
-			buffu = await getBaper(`https://api-self.herokuapp.com/docs/canvaswel?name=${pushname}&img_url=${data.display_url}&gcname=${group.subject}&jumlahmem=${group.participants.length}&apikey=${apikey}`)
+			buffu = await getBaper(`https://api-self.herokuapp.com/docs/canvaswel?name=${pushname}&img_url=${data.display_url}&gcname=${group.subject}&jumlahmem=${group.participants.length}`)
 				benny.sendMessage(group.id, buffu, MessageType.image, {contextInfo: {mentionedJid: [num]}, caption: txtwl, quoted: { "key": { "participant": `${numbernye}`, "remoteJid": `${setgrup}`, "fromMe": false, "id": "B391837A58338BA8186C47E51FFDFD4A" }, "message": { "documentMessage": { "jpegThumbnail": buff, "mimetype": "application/octet-stream", "title": `*Welcome*`, "fileLength": "36", "pageCount": 0, "fileName": `_*Welcome*_` }}, "messageTimestamp": "1614069378", "status": "PENDING"}})
 				} else if (anu.action == 'remove') {
 					if (!welcome.includes(anu.jid)) return
@@ -43,7 +43,7 @@ benny.on('group-participants-update', async (anu) => {
 					fs.writeFileSync('janckuk.jpeg', datae, 'base64')
 					var imgbb = require('imgbb-uploader')
 					data = await imgbb("cedeb44b8d204947a6833ca1412ca77d", 'janckuk.jpeg')
-				buffu = await getBaper(`https://api-self.herokuapp.com/docs/canvasbye?name=${pushname}&img_url=${data.display_url}&gcname=${group.subject}&jumlahmem=${group.participants.length}&apikey=${apikey}`)
+				buffu = await getBuffer(`https://api-self.herokuapp.com/docs/canvasbye?name=${pushname}&img_url=${data.display_url}&gcname=${group.subject}&jumlahmem=${group.participants.length}`)
 				benny.sendMessage(group.id, buffu, MessageType.image, {contextInfo: {mentionedJid: [num]}, caption: teks})
 				}
 } catch (e) {
